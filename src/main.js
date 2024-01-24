cf5683648d493f1d123de6d8ac9a65c94c1372ee
Lucas froidefond <lucas.froidefond@mmibordeaux.com>
2024-01-24 12:16:16 +0100

    start anim

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
+import gsap from "gsap";
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI()
+const gui = new GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Textures
  */
-
 
 /**
  * Object
@@ -29,8 +28,8 @@ scene.add(cube);
 
 // Cube
 const leftFace = new THREE.Mesh(
-    new THREE.PlaneGeometry(3, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000,  side: THREE.DoubleSide})
+  new THREE.PlaneGeometry(3, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide })
 );
 leftFace.position.x = -1;
 leftFace.position.y = -0.5;
@@ -38,8 +37,8 @@ leftFace.rotation.y = Math.PI / 2;
 cube.add(leftFace);
 
 const rightFace = new THREE.Mesh(
-    new THREE.PlaneGeometry(3, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide})
+  new THREE.PlaneGeometry(3, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
 );
 rightFace.position.x = 1;
 rightFace.position.y = -0.5;
@@ -47,105 +46,115 @@ rightFace.rotation.y = Math.PI / 2;
 cube.add(rightFace);
 
 const bottomFace = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 3),
-    new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide})
+  new THREE.PlaneGeometry(2, 3),
+  new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide })
 );
 bottomFace.rotation.x = Math.PI / 2;
 bottomFace.position.y = -1;
 cube.add(bottomFace);
 
 const frontFace = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff00ff, side: THREE.DoubleSide})
+  new THREE.PlaneGeometry(2, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff00ff, side: THREE.DoubleSide })
 );
 frontFace.position.y = -0.5;
 frontFace.position.z = 1.5;
 cube.add(frontFace);
 
 const backFace = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide})
+  new THREE.PlaneGeometry(2, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide })
 );
 backFace.position.y = -0.5;
 backFace.position.z = -1.5;
 cube.add(backFace);
 
+cube.rotation.y = Math.PI * 0.5;
+
 // CinemaTicket
 
 const cinemaTicket = new THREE.Mesh(
-    new THREE.PlaneGeometry(0.5, 1),
-    new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide})
+  new THREE.PlaneGeometry(0.5, 1),
+  new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide })
 );
 
 cinemaTicket.position.x = 0.2;
 cinemaTicket.position.y = -0.4;
 cinemaTicket.position.z = 0.3;
-cinemaTicket.rotation.x = Math.PI / 2 + 0.3 ;
+cinemaTicket.rotation.x = Math.PI / 2 + 0.3;
 
 scene.add(cinemaTicket);
-
-
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 4
-camera.position.y = 2
-camera.position.z = 5
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 1;
+camera.position.z = 7;
+
+// camera.position.y = 3;
+// camera.position.z = 2;
+
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
+//Animate
+
+gsap.to(camera.position, { duration: 4, delay: 0, z: 2 });
+gsap.to(camera.position, { duration: 4, delay: 0, y: 3 });
+// gsap.to(camera.rotation, { duration: 1, delay: 0, x: -Math.PI * 0.25 });
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
 function animate() {
+  const elapsedTime = clock.getElapsedTime();
 
-    const elapsedTime = clock.getElapsedTime()
-    
-    
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-    
-	renderer.render( scene, camera );
+  camera.lookAt(cube.position);
 
-	requestAnimationFrame( animate );
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
 }
 
 animate();
